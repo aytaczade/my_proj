cfc3ab10446285a629619d2ed2784f17d1d887f8
Aytac Mustafazade <aytacmustafazade@Aytacs-MacBook-Air.local>
2023-09-07 01:46:39 +0400

    context

--- course_project/core/views.py
+++ course_project/core/views.py
@@ -1,39 +1,39 @@
-from django.shortcuts import redirect, render
-from django.urls import reverse_lazy
-from about.models import About
-from product.models import Category,Product,Campaign
-from about.models import Store
 from django.http import HttpResponseRedirect
-from .forms import ContactForm
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from product.models import Category, Product, Campaign
+from about.models import About, Store
 from django.db.models import Q
 from django.contrib import messages
+from .forms import ContactForm
 
 
 def index(request):
-    context={
-      'trandy_products':Product.objects.filter(is_trand=True)[:8],
-      'just_arrived_products':Product.objects.all().order_by('-created_at')[:8],
-      'campaigns':Campaign.objects.all()[:2]
+    context = {
+        'trandy_products': Product.objects.filter(is_trand=True)[:8],
+        'just_arrived_products': Product.objects.all().order_by('-created_at')[:8],
+        'campaigns': Campaign.objects.all()[:2],
     }
-    return render(request,'home/index.html', context)
+    return render(request, 'home/index.html', context)
 
 
 def contact(request):
-    stores=Store.objects.all()
+    stores = Store.objects.all()
     form = ContactForm()
-    
+
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Müraciətiniz uğurla qeydə alındı, tezliklə sizinlə əlaqə saxlanılacaq')
-            return redirect(reverse_lazy("contact"))
-        
-    context={
-        'stores':stores,
+            return redirect(reverse("contact"))
+
+    context = {
+        'stores': stores,
         'form': form,
     }
-    return render(request,'contact/index.html', context)
+    return render(request, 'contact/index.html', context)
+
 
 def search(request):
     query = request.GET.get('search')
@@ -50,11 +50,10 @@ def search(request):
 
     return render(request, 'product/search.html', context)
 
+
 def about(request):
-    about=About.objects.first()
-    context={
-        'about':about
+    about = About.objects.first()
+    context = {
+        'about': about,
     }
-    return render(request,'about/index.html', context)
-    
-    
+    return render(request, 'about/index.html', context)
